12108f993e1b579248af4b577e1b1459998219f7
Max <ivannikov.maxim@seznam.cz>
2021-09-20 22:47:27 +0200

    seventh

--- src/services/resto-service.js
+++ src/services/resto-service.js
@@ -24,6 +24,31 @@ export default class RestoService {
             return item;
         }
 
+        async setOrder(order) {
+            const number = await this.getOrderNumber();
+            const newOrder = {
+                id: number,
+                order: order
+            }
+            const response = await fetch(`${this._apiBase}/orders`, {
+                method: 'POST', 
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify(newOrder)
+            });
+            if (!response.ok){
+                throw new Error('json error'); 
+            }
+        }
+    
+        async getOrderNumber(){
+            const res = await this.getResource('/orders/');
+            const orderNumber = res.length+1;
+    
+            return orderNumber
+        }
+
     // getMenuItems = async () => {
     //     const response = await fetch(this.url);
     //     if(!response.ok) {
